cbea7264c16fb6114960e0629de8c98885df577c
Irina <104649475+IrinaRen@users.noreply.github.com>
2023-05-26 22:03:25 +0300

    Update js_hm1_31.js

--- js_hm1_31.js
+++ js_hm1_31.js
@@ -61,11 +61,11 @@
  item_6_type = typeof(item_6);
 
 //  21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——
- console.log('item_6 == '+item_6,'item_6_type == '+item_6_type);
+ console.log('item_6 == '+item_6,' item_6_type == '+item_6_type);
  
 //  22. Создать переменную item_7 и в ней преобразовать item_6 в String:
- let_7 = String(item_6);
- let item_7 = "item_6"; (?)
+ let item_7 = String(item_6);
+//  let item_7 = "item_6"; (?)
 
 //  23. Создать переменную item_7_type:
  let item_7_type;
@@ -90,8 +90,7 @@ if (age_1 < age_2) {
     console.log('You don’t have access cause your age is ' + age_1 + ' It’s less then ')};
 
 // 30. Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is ” + age_1 + “ It’s less then ”
-else if (age_1 >= age_2 && age_1 < age_3) {
-    console.log('Welcome  !')}; 
+if (age_1 >= age_2 && age_1 < age_3) { console.log('Welcome  !')}; 
 
 //  31. Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome  !”:
  if(age_1 >=  age_2 && age_1 < 60) [console.log(' Welcome  ! ')];
@@ -185,6 +184,25 @@ checkAge3("17");
 checkAge3("18a");
 checkAge3("aaa");
 
+// const checkAge3 = (age_1) => {
+//     if (typeof age_1 != 'number') {
+//         let ageNew = +typeof(age_1)
+//     }
+// if (age_1 < age_2) {
+//     console.log("You don't have access cause your age is " + age_1 + "It's less then ")
+// }  else if (age_1 >= age_2 && age_1 < age_3) {
+//     console.log("Welcome  !")
+// }
+//  else if (age_1  > age_3) {
+//     console.log("Keep calm and look Culture channel")
+// } else {
+//     console.log("Technical work")
+// }
+// }
+// checkAge3("17");
+// checkAge3("18a");
+// checkAge3("aaa");
+
 // Задача со звёздочкой 4***
 // Преобразовать задание 3* таким образом, чтобы возраст вводится используя функцию prompt в привязанной верстке
 
@@ -210,7 +228,6 @@ function checkAge4(age) {
 }
 
 checkAge4(prompt('Input your age: ', [18]));
-
 
 
 
